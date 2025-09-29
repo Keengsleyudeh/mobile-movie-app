67c2e2661d1f7b9ed72b2341a05e26a8598c5a4b
keengsleyudeh <keengsleyudeh@gmail.com>
2025-09-29 22:18:20 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Updated package.json to include nativewind and tailwindcss dependencies.
    - Removed reset-project.js script as it is no longer needed.
    - Added tailwind.config.js for Tailwind CSS configuration with custom theme colors.
    -implemented tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,117 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+import { Image, ImageBackground } from "react-native";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const TabIcon = ({focused, icon, title}: any) => {
+  if (focused) {
+    return (
+          <ImageBackground
+              source={images.highlight}
+              className="flex flex-row w-full flex-1 min-w-[112px] min-h-14
+              justify-center items-center rounded-full overflow-hidden"
+          >
+              <Image source={icon}
+              tintColor={"#151312"}
+              className="size-5" />
 
+              <Text className="text-base text-secondary font-semibold ml-2">{title}</Text>
+          </ImageBackground>
+    )
+  } else {
+    return (
+      <View className="size-full justify-center items-center rounded-full">
+          <Image source={icon}
+            tintColor={"#A8B5DB"}
+            className="size-5" 
+          />
+      </View>
+    )
+  }
+}
+
+export default function RootLayout() {
   return (
-    <Tabs
+    <Tabs 
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarStyle: {
+          backgroundColor: '#0f0D23',
+          borderRadius: 50,
+          marginHorizontal: 20,
+          height: 52,
+          position: 'absolute',
+          overflow: 'hidden',
+          borderWidth: 1,
+          borderColor: '0f0d23',
+          
+        }
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+          headerShown: false,
+        title: "Home",
+        tabBarIcon: ({focused})=> (
+            <TabIcon 
+              focused={focused}
+              icon={icons.home}
+              title="Home"
+            />
+        )
+     }}
+    />
+    <Tabs.Screen
+      name="search"
+      options={{ 
+        headerShown: false, 
+        title: "Search",
+        tabBarIcon: ({focused})=> (
+            <TabIcon 
+              focused={focused}
+              icon={icons.search}
+              title="Search"
+            />
+        )
+     }}
+    />
+    <Tabs.Screen
+      name="saved"
+      options={{ 
+        headerShown: false, 
+        title: "Saved",
+        tabBarIcon: ({focused})=> (
+            <TabIcon 
+              focused={focused}
+              icon={icons.save}
+              title="Saved"
+            />
+        )
+     }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{ 
+        headerShown: false, 
+        title: "Profile",
+        tabBarIcon: ({focused})=> (
+            <TabIcon 
+              focused={focused}
+              icon={icons.person}
+              title="Profile"
+            />
+        )
+     }}
+    />
+  </Tabs>
   );
 }
